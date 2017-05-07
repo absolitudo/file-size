c19554375d39d16043c5eb9f71a78cf5e7d053a1
absolitudo <absolitudo@gmail.com>
2017-05-07 15:04:10 +0200

    upload files to uploads dir get file size send json back with filesize delete file from uploads dir

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-var express = require("express"),
+var fs = require("fs"),
+    express = require("express"),
     app = express(),
-    multer = require("multer");
+    multer = require("multer"),
+    upload = multer({dest: "uploads/"});
 
 app.use(express.static("./static"));
+
+app.post("/upload", upload.single("file"), function(req, res) {
+    res.send({
+        "size": fs.statSync("uploads/" + req.file.filename).size
+    });
+    fs.unlink("uploads/" + req.file.filename);
+});
 app.listen(process.env.PORT || 8080);
 console.log("server up");
